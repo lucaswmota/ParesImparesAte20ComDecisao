b95250da4fd7f83f92484177d33a6d3e8cea5dfa
Lucas Mota <lucasmota7@gmail.com>
2021-11-29 20:06:41 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParesImparesAte20ComDecisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("digite 1 para numeros impares ou 2 para numeros pares.");
+            int numero = int.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    while (numero <= 20)
+                    {
+                        Console.WriteLine(numero);
+                        numero += 2;
+                        
+                    }
+                    break;
+                case 2:
+                    while (numero <= 20)
+                    {
+                        Console.WriteLine(numero);
+                        numero += 2;
+                        
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Resposta Incorreta.");
+                    break;
+            }
+
+          
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
